b2c8d45578f0bd786920f628df23ab1d435bde60
Mohit Ramani <ramanimohit01@gmail.com>
2022-11-27 23:28:54 +0530

    added CardFront component

--- interactive-card-details-form/src/App.jsx
+++ interactive-card-details-form/src/App.jsx
@@ -1,11 +1,13 @@
-
+import CardFront from './Components/CardFront'
 function App() {
-
-  return (
-    <div className="App">
-
-    </div>
-  )
+    return (
+        <div className="App flex h-screen w-screen flex-col md:flex-row">
+            <div className="relative w-full h-1/3 md:w-1/3 md:h-screen bg-cover bg-no-repeat bg-[url('./assets/bg-main-mobile.png')] md:bg-[url('./assets/bg-main-desktop.png')]">
+                <CardFront />
+            </div>
+            <div className="w-full h-2/3 md:w-2/3 md:h-screen bg-white"></div>
+        </div>
+    )
 }
 
 export default App
---
+++ interactive-card-details-form/src/Components/CardFront.jsx
@@ -0,0 +1,7 @@
+export default function CardFront({}) {
+    return (
+        <>
+            <div className="relative top-[60%] left-[4%] md:top-[15%] md:left-[25%] lg:left-[40%] h-32 w-56 md:h-52 md:w-96 bg-contain  bg-no-repeat bg-[url('./assets/bg-card-front.png')]"></div>
+        </>
+    )
+}
